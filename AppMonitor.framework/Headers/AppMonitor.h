577f37d5c8f5bb972f2f87008bfc85678345560f
RAHUL CK <rahulvatakara@gmail.com>
2016-11-04 03:45:51 +0530

    committing framework

---
+++ AppMonitor.framework/Headers/AppMonitor.h
@@ -0,0 +1,19 @@
+//
+//  AppMonitor.h
+//  AppMonitor
+//
+//  Created by RAHUL CK on 11/3/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppMonitor.
+FOUNDATION_EXPORT double AppMonitorVersionNumber;
+
+//! Project version string for AppMonitor.
+FOUNDATION_EXPORT const unsigned char AppMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppMonitor/PublicHeader.h>
+
+
