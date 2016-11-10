64d9368ab711e65f3bd4693c07936c550d2b4fe7
RAHUL CK <rahulvatakara@gmail.com>
2016-11-10 16:53:03 +0530

    version 1.4

--- Source/AppMonitor/build/AppMonitor.build/Debug-iphoneos/AppMonitor.build/Objects-normal/armv7/AppMonitorEvent.d
+++ Source/AppMonitor/build/AppMonitor.build/Debug-iphoneos/AppMonitor.build/Objects-normal/armv7/AppMonitorEvent.d
@@ -3,4 +3,5 @@ dependencies: \
   /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/AppMonitor/Classes/Event/AppMonitorEvent.h \
   /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/AppMonitor/Classes/Logging/AppMonitorLogger.h \
   /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/build/AppMonitor.build/Debug-iphoneos/AppMonitor.build/module.modulemap \
-  /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/AppMonitor/AppMonitor.h
+  /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/AppMonitor/AppMonitor.h \
+  /Volumes/Development/Assigment/AppMonitor/AppMonitor/Source/AppMonitor/AppMonitor/Classes/Constants/AppMonitorContants.h
